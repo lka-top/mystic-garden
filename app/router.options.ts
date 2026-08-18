b1c8d6159bdb06d348ed4408ca40725947563546
lka-top <likai8895@163.com>
2026-08-18 19:41:59 +0800

    feat(transition): implement Mizuki-style SPA slide-in page transition from right

---
+++ app/router.options.ts
@@ -0,0 +1,19 @@
+import type { RouterConfig } from '@nuxt/schema'
+
+export default <RouterConfig>{
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+
+    if (to.hash) {
+      return {
+        el: to.hash,
+        top: 80,
+        behavior: 'smooth'
+      }
+    }
+
+    return { top: 0, left: 0 }
+  }
+}
